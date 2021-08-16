026e6f404f76e38e524cd42050c9063c2432c0ce
benevenuto30 <rafaelmaster03@gmail.com>
2021-08-16 12:45:17 -0300

    updates cadastro

---
+++ cypress/integration/cadastro.spec.js
@@ -0,0 +1,69 @@
+/// <reference types="cypress" />
+let Chance = require('chance');
+let chance = new Chance();
+context('Cadastro', () => {
+    it('Cadastro de usuario no site', () => {
+
+        //rotas
+        //GET /api/1/databases/userdetails/collections/newtable?apiKey=YEX0M2QMPd7JWJw_ipMB3a5gDddt4B_X
+        //POST /api/1/databases/userdetails/collections/newtable?apiKey=YEX0M2QMPd7JWJw_ipMB3a5gDddt4B_X
+        //POST /api/1/databases/userdetails/collections/usertable?apiKey=YEX0M2QMPd7JWJw_ipMB3a5gDddt4B_X
+
+        cy.server()
+        cy.route({
+          method: 'POST',
+          url: '**/api/1/databases/userdetails/collections/newtable?**',
+          status: 200,
+          response: {}
+        }).as('postNewtable');
+        
+        cy.route({
+            method: 'POST', 
+            url: '**/api/1/databases/userdetails/collections/usertable?**', 
+            status: 200, 
+            response: {}
+          }).as('postUsertable');
+        
+        cy.route({
+          method: 'GET',
+          url: '**/api/1/databases/userdetails/collections/newtable?**',
+          status: 200,
+          response: {}
+          }).as('getNewtable');
+
+       cy.visit('Register.html');
+       cy.get('input[placeholder="First Name"]').type(chance.first());
+       cy.get('input[ng-model^=Last]').type(chance.last());
+       cy.get('input[ng-model^=Email]').type(chance.email());
+       cy.get('input[ng-model^=Phone]').type(chance.phone({formatted: false}));
+       cy.get('input[value=Male]').check();
+       cy.get('input[type=checkbox]').check("Cricket");
+       cy.get('input[type=checkbox]').check("Hockey");
+       cy.get('select#Skills').select('APIs');
+       cy.get('select#countries').select('Brazil');
+       cy.get('select#country').select('Australia',{force: true});
+       cy.get('select#yearbox').select('1975');
+       cy.get('select[ng-model^=month]').select('September');
+       cy.get('select#daybox').select('1');
+       cy.get('input#firstpassword').type('Ab@2021');
+       cy.get('input#secondpassword').type('Ab@2021');
+       cy.get('input#imagesrc').attachFile('Capturar.PNG');
+       cy.get('button#submitbtn').click();
+
+       cy.wait('@postNewTable').then((resNewTable)=>{
+           expect(resNewTable.response.status).to.eq(200)
+           cy.log(resNewTable.response.status)
+       })
+       cy.wait('@postUserTable').then((resNewTable)=>{
+           expect(resNewTable.response.status).to.eq(200)
+       })
+       cy.wait('@getNewTable').then((resNewTable)=>{
+           expect(resNewTable.response.status).to.eq(200)
+       })
+
+       cy.url().should('contain','WebTable')
+    });
+});
+
+
+
